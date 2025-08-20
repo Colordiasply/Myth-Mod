a9474f1b68f00cf3b8b4662feb0e788b4bdf0409
Colordiasply <colordisply@gmail.com>
2025-08-20 03:06:45 -0400

    Update 1

--- src/main/java/colormaya/mythosmod/mod/MythosMod.java
+++ src/main/java/colormaya/mythosmod/mod/MythosMod.java
@@ -12,6 +12,7 @@ public class MythosMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
         ModItems.registerModItems();
 	}
 }
--- src/main/java/colormaya/mythosmod/mod/item/ModItems.java
+++ src/main/java/colormaya/mythosmod/mod/item/ModItems.java
@@ -1,9 +1,27 @@
 package colormaya.mythosmod.mod.item;
 
 import colormaya.mythosmod.mod.MythosMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item LIMESTONE_CHUNK = registerItem("limestone_chunk", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(MythosMod.MOD_ID, name), item);
+    }
+
     public static void registerModItems() {
+        MythosMod.LOGGER.info("Registering Mod Items for " + MythosMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(LIMESTONE_CHUNK);
+
+        });
     }
 }
 
