8366fa1d33459afcf242c03d8ee5b93e4dbf1756
MainlyMaya <superjussi2009@gmail.com>
2025-08-21 00:28:43 +0200

    V-0.0.1

--- src/main/java/colormaya/mythosmod/mod/item/ModItems.java
+++ src/main/java/colormaya/mythosmod/mod/item/ModItems.java
@@ -1,8 +1,10 @@
 package colormaya.mythosmod.mod.item;
 
 import colormaya.mythosmod.mod.MythosMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -10,6 +12,13 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item LIMESTONE_CHUNK = registerItem("limestone_chunk", new Item(new Item.Settings()));
+
+    public static final Item RoseGold_Ingot = registerItem("rosegold_ingot", new Item(new Item.Settings()));
+
+    public static final Item RoseGold_Nugget = registerItem("rosegold_nugget", new Item(new Item.Settings()));
+
+    public static final Item Raw_RoseGold = registerItem("raw_rosegold", new Item(new Item.Settings()));
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(MythosMod.MOD_ID, name), item);
@@ -20,6 +29,10 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(LIMESTONE_CHUNK);
+            entries.add(RoseGold_Ingot);
+            entries.add(RoseGold_Ingot);
+            entries.add(RoseGold_Nugget);
+            entries.add(Raw_RoseGold);
 
         });
     }
