0903faebab68dfffe51777119df5b6b4310b56be
Colordiasply <colordisply@gmail.com>
2025-08-24 11:51:22 -0400

    Color Limestone itself

--- src/main/java/colormaya/mythosmod/mod/block/ModBlocks.java
+++ src/main/java/colormaya/mythosmod/mod/block/ModBlocks.java
@@ -4,6 +4,8 @@ import colormaya.mythosmod.mod.MythosMod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.enums.Instrument;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -13,6 +15,12 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
+    public static final Block LIMESTONE_BRICKS = registerBlock("limestone_bricks",
+            new Block(AbstractBlock.Settings.create().requiresTool().strength(1.5F, 2.0F).sounds(BlockSoundGroup.DRIPSTONE_BLOCK).mapColor(MapColor.TERRACOTTA_BROWN).instrument(Instrument.BASEDRUM)));
+    public static final Block CHISELED_LIMESTONE_BRICKS = registerBlock("chiseled_limestone_bricks",
+            new Block(AbstractBlock.Settings.create().requiresTool().strength(1.5F, 2.0F).sounds(BlockSoundGroup.DRIPSTONE_BLOCK).mapColor(MapColor.TERRACOTTA_BROWN).instrument(Instrument.BASEDRUM)));
+    static final Block LIMESTONE = registerBlock("limestone",
+            new Block(AbstractBlock.Settings.create().requiresTool().strength(1.5F, 2.0F).sounds(BlockSoundGroup.DRIPSTONE_BLOCK).mapColor(MapColor.TERRACOTTA_BROWN).instrument(Instrument.BASEDRUM)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -27,6 +35,10 @@ public class ModBlocks {
         MythosMod.LOGGER.info("Registering Mod Blocks for " + MythosMod.MOD_ID);
 
             ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+                entries.add(LIMESTONE);
+                entries.add(LIMESTONE_BRICKS);
+                entries.add(CHISELED_LIMESTONE_BRICKS);
+
             });
         }
     }
